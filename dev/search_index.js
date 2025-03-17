f2bdd59cc90655defe1a379058fc0a56b3680650
Documenter.jl <documenter@juliadocs.github.io>
2025-03-17 19:30:29 +0000

    build based on 27d27fb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LMDiskANN","category":"page"},{"location":"#LMDiskANN","page":"Home","title":"LMDiskANN","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LMDiskANN.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LMDiskANN]\nPrivate = false\nOrder = [:function]","category":"page"}]
+}
